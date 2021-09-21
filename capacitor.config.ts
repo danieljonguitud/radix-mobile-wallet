16d989dfcc59abb65097e4fcb634e062845ad8dc
Daniel Jonguitud <danieljonguitud17@outlook.com>
2021-09-21 10:13:59 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'radix-wallet',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
